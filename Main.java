5ec1840bc694eb4d52349278da54dd5a4fc1eaba
Romann1234 <145775621+Romann1234@users.noreply.github.com>
2023-09-27 21:16:46 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,50 @@
+package p1;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int num[];
+        num = new int[5];
+        Random d = new Random();
+int changecounter = 0;
+int changecounter2 = 0;
+int changecounter3 = 0;
+ int S = 0;
+ int D = 0;
+        for (int i=0;i<num.length;i++){
+            num[i]=50-d.nextInt(101);
+
+            if(num[i]<0) {
+                changecounter++;
+            }
+            System.out.println(num[i]);
+            if(num[i]>0) {
+                changecounter2++;
+            }
+            if(num[i]==0) {
+                changecounter3++;
+            }
+            if(num[i]>0) {
+                S = S +num[i];
+            }
+            if(num[i]<0) {
+                D = D +num[i];
+
+            }
+
+        }
+        System.out.println("col-vo - elementov:  "+ changecounter);
+        System.out.println("col-vo + elementov:  "+ changecounter2);
+        System.out.println("col-vo - elementov:  "+ changecounter3);
+        System.out.println("Summa + elementov: " +S );
+        System.out.println("Summa - elementov: " +D );
+        System.out.printf("srednee: %d  ",((S+D)/(changecounter+changecounter2)));
+
+
+
+    }
+
+}
+
